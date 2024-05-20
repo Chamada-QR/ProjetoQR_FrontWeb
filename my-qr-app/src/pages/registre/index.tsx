edea9e65d2d75fa11a78c0cd09b5716a6677dcb4
77Eduardo <103968776+77Eduardo@users.noreply.github.com>
2024-05-20 19:45:42 -0300

    feat #2 TelaRegistrar

--- my-qr-app/src/pages/registre/index.tsx
+++ my-qr-app/src/pages/registre/index.tsx
@@ -4,12 +4,25 @@ import { Link } from 'react-router-dom'
 function Registre(){
     return(
         <section className={styles.fundo_registre}>
-            <Link to='/'>
-                <button>VOLTAR </button>
-            </Link>
+
+            <div className={styles.loginRegistre}>             
+                <div>
+                    <input className={styles.email} type="email" name="email" placeholder='Nome'/>  
+
+                    <input className={styles.ra} type='text' name="number" placeholder='Ra'/>      
+                </div>
+                <Link to='/'>
+                    <button className={styles.button_voltar}>VOLTAR </button>
+                </Link>
+            </div>
 
         </section>
     );
 };
 
 export default Registre;
+
+<section className={styles.fundo}>
+
+
+</section>
